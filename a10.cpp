04c9dd6ddb4de2337affea88e6198832122d786f
evlrhawk <36744649+evlrhawk@users.noreply.github.com>
2018-07-17 14:10:34 -0600

    Add files via upload

---
+++ a10.cpp
@@ -0,0 +1,28 @@
+/***********************************************************************
+* Program:
+*    Assignment 10, Hello World Assignment
+*    Brother Comeau, CS124
+* Author:
+*    Thomas Burr
+* Summary: 
+*    This is a program that displays Hello World on the screen.
+*
+*    Estimated:  0.0 hrs   
+*    Actual:     0.0 hrs
+*      Please describe briefly what was the most difficult part.
+************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+ * Main Function that Displays Hello World
+ ***********************************************************************/
+int main()
+{
+   // display
+   cout << "Hello World\n";
+   int row = 'A';
+   cout << row;
+   return 0;
+}
